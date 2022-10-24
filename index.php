ee2da323483ec9a37926239d086f7f4249d894d8
atikaummul <atikaummul02@gmail.com>
2022-10-24 11:01:00 +0700

    initial commit

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php
+
+    echo "Hello World! <br>";
+
+    $nama = "Atika Ummul Azizia";
+    $umur = "20";
+
+    echo "Nama saya <strong>$nama</strong>, saya berusia $umur tahun <br>";
+
+    $namaAdik = "Riko";
+    $umurAdik = "17";
+
+    $selisihUmur = $umur - $umurAdik;
+
+    echo "Nama Adik saya <strong>$namaAdik</strong>, adik saya berusia $umurAdik tahun <br>";
+
+    echo "Selisih umur saya dan adik saya adalah $selisihUmur tahun";
+
+?>
