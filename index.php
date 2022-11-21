ff908ab6f59935d94c7f2bbe609190efca6c4ffc
atikaummul <atikaummul02@gmail.com>
2022-11-21 11:57:19 +0700

    phpcrud

---
+++ index.php
@@ -0,0 +1,65 @@
+<?php
+// 1. Buat koneksi dengan MYSQL
+$con = mysqli_connect("localhost","root","","fakultas");
+
+// 2. Cek koneksi dengan MSQL
+if(mysqli_connect_errno()){
+    echo "Koneksi gagal" . mysqli_connect_error();
+}else{
+    echo "Koneksi berhasil";
+}
+
+// 3 buat query baca semua data dari table
+$sql = "SELECT * FROM mahasiswa";
+
+// 4. tampilkan data, cek apakah query bisa dijalankan
+$mahasiswa = [];
+if ($result = mysqli_query($con, $sql)) {
+    // tampilkan satu per satu
+    while ($row = mysqli_fetch_assoc($result)) {
+        $mahasiswa[] = $row;
+    }
+    mysqli_free_result($result);
+  }
+
+// 5. tutup koneksi
+mysqli_close($con);
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Data Mahasiswa</title>
+</head>
+<body style="background-color: #D8BFD8">
+    <h1>Data Mahasiswa</h1>
+    <a href="insert.php">Tambah Data</a>
+    <table border=1 style="width: 100%;">
+        <tr>
+            <th>NIM</th>
+            <th>Nama</th>
+            <th>Jenis Kelamin</th>
+            <th>Tempat Lahir</th>
+            <th>Tanggal Lahir</th>
+            <th>Alamat</th>
+        </tr>
+        <?php foreach ($mahasiswa as $row): ?>
+            <tr>
+                <td><?= $row['nim'] ?></td>
+                <td><?= $row['nama'] ?></td>
+                <td><?= $row['jenis_kelamin'] ?></td>
+                <td><?= $row['tempat_lahir'] ?></td>
+                <td><?= $row['tanggal_lahir'] ?></td>
+                <td><?= $row['alamat'] ?></td>
+                <td>
+                    <a href="update.php?id=<?= $row['id'] ?>" >Edit</a> | 
+                    <a href="delete.php?id=<?= $row['id'] ?>" >Delete</a>
+                </td>
+            </tr>
+        <?php endforeach; ?>
+    </table>
+</body>
+</html>
